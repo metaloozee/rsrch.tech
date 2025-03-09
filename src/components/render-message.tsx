9646bd82f1d46cbcb661a4dff026af952714b672
metaloozee <ayanparkar123@gmail.com>
2025-03-09 14:41:22 +0530

    basic tool interface

--- src/components/render-message.tsx
+++ src/components/render-message.tsx
@@ -1,7 +1,9 @@
-import * as React from 'react';
-import { Message } from 'ai';
+import { useMemo, memo } from 'react';
+import { Message, ToolInvocation } from 'ai';
 import { UserMessage } from '@/components/chat-message';
 import { BotMessage } from '@/components/chat-message';
+import { Badge } from '@/components/ui/badge';
+import { LoaderCircleIcon } from 'lucide-react';
 
 interface RenderMessageProps {
     message: Message;
@@ -12,7 +14,21 @@ interface RenderMessageProps {
     chatId?: string;
 }
 
-export function RenderMessage({
+const ToolState = memo(({ state, name }: { state: string; name: string }) => {
+    if (state === 'call') {
+        return (
+            <Badge variant={'outline'}>
+                <LoaderCircleIcon className="size-3" /> Running {name}
+            </Badge>
+        );
+    }
+
+    return <Badge variant={'outline'}>Done</Badge>;
+});
+
+ToolState.displayName = 'ToolState';
+
+export const RenderMessage = memo(function RenderMessage({
     message,
     messageId,
     getIsOpen,
@@ -20,9 +36,43 @@ export function RenderMessage({
     onQuerySelect,
     chatId,
 }: RenderMessageProps) {
+    const toolData = useMemo(() => {
+        if (message.role !== 'assistant' || !message.parts) {
+            return null;
+        }
+
+        const toolAnnotations = message.parts.filter((part) => part.type === 'tool-invocation');
+        if (toolAnnotations.length === 0) {
+            return null;
+        }
+
+        return Array.from(
+            toolAnnotations.reduce((acc, tool: (typeof toolAnnotations)[0]) => {
+                const { toolCallId, state } = tool.toolInvocation;
+                if (!acc.has(toolCallId) || state === 'result') {
+                    acc.set(toolCallId, tool.toolInvocation);
+                }
+                return acc;
+            }, new Map<string, ToolInvocation>())
+        ).map(([_, value]) => value);
+    }, [message.role, message.parts]);
+
     if (message.role === 'user') {
         return <UserMessage message={message.content} />;
     }
 
-    return <BotMessage message={message.content} />;
-}
+    return (
+        <>
+            {toolData && toolData.length > 0 && (
+                <div className="flex flex-row gap-2">
+                    {toolData.map((tool) => (
+                        <ToolState key={tool.toolCallId} name={tool.toolName} state={tool.state} />
+                    ))}
+                </div>
+            )}
+            <BotMessage message={message.content} />
+        </>
+    );
+});
+
+RenderMessage.displayName = 'RenderMessage';
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,182 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {
+    generateId,
+    type CoreAssistantMessage,
+    type CoreMessage,
+    type CoreToolMessage,
+    type Message,
+    type TextStreamPart,
+    type ToolInvocation,
+    type ToolSet,
+} from 'ai';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+function addToolMessageToChat({
+    toolMessage,
+    messages,
+}: {
+    toolMessage: CoreToolMessage;
+    messages: Array<Message>;
+}): Array<Message> {
+    return messages.map((message) => {
+        if (message.parts) {
+            return {
+                ...message,
+                toolInvocations: message.parts
+                    .filter((p) => p.type === 'tool-invocation')
+                    .map((part) => {
+                        const toolResult = toolMessage.content.find(
+                            (tool) => tool.toolCallId === part.toolInvocation.toolCallId
+                        );
+
+                        if (toolResult) {
+                            return {
+                                ...part.toolInvocation,
+                                state: 'result',
+                                result: toolResult.result,
+                            };
+                        }
+
+                        return part.toolInvocation;
+                    }),
+            };
+        }
+
+        return message;
+    });
+}
+
+export function convertToUIMessages(messages: Array<CoreMessage>): Array<Message> {
+    return messages.reduce((chatMessages: Array<Message>, message) => {
+        if (message.role === 'tool') {
+            return addToolMessageToChat({
+                toolMessage: message as CoreToolMessage,
+                messages: chatMessages,
+            });
+        }
+
+        let textContent = '';
+        let reasoning: string | undefined = undefined;
+        const toolInvocations: Array<ToolInvocation> = [];
+
+        if (typeof message.content === 'string') {
+            textContent = message.content;
+        } else if (Array.isArray(message.content)) {
+            for (const content of message.content) {
+                if (content.type === 'text') {
+                    textContent += content.text;
+                } else if (content.type === 'tool-call') {
+                    toolInvocations.push({
+                        state: 'call',
+                        toolCallId: content.toolCallId,
+                        toolName: content.toolName,
+                        args: content.args,
+                    });
+                } else if (content.type === 'reasoning') {
+                    reasoning = content.text;
+                }
+            }
+        }
+
+        chatMessages.push({
+            id: generateId(),
+            role: message.role as Message['role'],
+            content: textContent,
+            reasoning,
+            toolInvocations,
+        });
+
+        return chatMessages;
+    }, []);
+}
+
+type ResponseMessageWithoutId = CoreToolMessage | CoreAssistantMessage;
+type ResponseMessage = ResponseMessageWithoutId & { id: string };
+
+export function sanitizeResponseMessages({
+    messages,
+    reasoning,
+}: {
+    messages: Array<ResponseMessage>;
+    reasoning: string | undefined;
+}) {
+    const toolResultIds: Array<string> = [];
+
+    for (const message of messages) {
+        if (message.role === 'tool') {
+            for (const content of message.content) {
+                if (content.type === 'tool-result') {
+                    toolResultIds.push(content.toolCallId);
+                }
+            }
+        }
+    }
+
+    const messagesBySanitizedContent = messages.map((message) => {
+        if (message.role !== 'assistant') return message;
+
+        if (typeof message.content === 'string') return message;
+
+        const sanitizedContent = message.content.filter((content) =>
+            content.type === 'tool-call'
+                ? toolResultIds.includes(content.toolCallId)
+                : content.type === 'text'
+                  ? content.text.length > 0
+                  : true
+        );
+
+        if (reasoning) {
+            // @ts-expect-error: reasoning message parts in sdk is wip
+            sanitizedContent.push({ type: 'reasoning', reasoning });
+        }
+
+        return {
+            ...message,
+            content: sanitizedContent,
+        };
+    });
+
+    return messagesBySanitizedContent.filter((message) => message.content.length > 0);
+}
+
+export function sanitizeUIMessages(messages: Array<Message>): Array<Message> {
+    const messagesBySanitizedToolInvocations = messages.map((message) => {
+        if (message.role !== 'assistant') return message;
+
+        if (!message.toolInvocations) return message;
+
+        const toolResultIds: Array<string> = [];
+
+        for (const toolInvocation of message.toolInvocations) {
+            if (toolInvocation.state === 'result') {
+                toolResultIds.push(toolInvocation.toolCallId);
+            }
+        }
+
+        const sanitizedToolInvocations = message.toolInvocations.filter(
+            (toolInvocation) =>
+                toolInvocation.state === 'result' ||
+                toolResultIds.includes(toolInvocation.toolCallId)
+        );
+
+        return {
+            ...message,
+            toolInvocations: sanitizedToolInvocations,
+        };
+    });
+
+    return messagesBySanitizedToolInvocations.filter(
+        (message) =>
+            message.content.length > 0 ||
+            (message.toolInvocations && message.toolInvocations.length > 0)
+    );
+}
+
+export function getMostRecentUserMessage(messages: Array<Message>) {
+    const userMessages = messages.filter((message) => message.role === 'user');
+    return userMessages.at(-1);
 }
