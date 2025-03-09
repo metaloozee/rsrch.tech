b669ea7a82ce066e173870f2b4bc2dc4a7dbdd86
metaloozee <ayanparkar123@gmail.com>
2025-03-09 17:42:57 +0530

    tool call result

--- src/components/tool.tsx
+++ src/components/tool.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { memo } from 'react';
-import { TextLoop } from '@/components/motion-primitives/text-loop';
-import { GlobeIcon, LoaderCircleIcon } from 'lucide-react';
+import { GlobeIcon, LoaderCircleIcon, SearchIcon } from 'lucide-react';
 import { TextShimmer } from './motion-primitives/text-shimmer';
+import { Separator } from './ui/separator';
 
 interface ToolProps {
     state: 'call' | 'result' | 'partial-call';
@@ -14,17 +14,51 @@ export const Tool = memo(({ state, name, results }: ToolProps) => {
     switch (name) {
         case 'web_search':
             return (
-                <div className="py-2 px-4 bg-neutral-900 rounded-lg font-light text-muted-foreground text-xs">
+                <div className="p-4 w-full bg-neutral-900 rounded-lg font-light text-muted-foreground text-xs">
                     {state === 'result' ? (
-                        <span className="flex gap-2 justify-center items-center">
-                            <GlobeIcon className="size-3" />
-                            {results?.reduce(
-                                (total: number, query: any) =>
-                                    total + (query.result?.results?.length || 0),
-                                0
-                            )}{' '}
-                            Sources
-                        </span>
+                        <div className="flex w-full flex-col justify-center items-center gap-4">
+                            <div className="flex w-full gap-2 justify-between items-center">
+                                <div className="flex justify-center items-center gap-2">
+                                    <GlobeIcon className="size-3" />
+                                    Web Search
+                                </div>
+                                <div>
+                                    {results?.reduce(
+                                        (total: number, query: any) =>
+                                            total + (query.result?.results?.length || 0),
+                                        0
+                                    )}{' '}
+                                    Sources
+                                </div>
+                            </div>
+                            <Separator />
+                            <div className="flex w-full flex-col gap-4 justify-center items-center">
+                                <div className="flex w-full gap-6 justify-start items-center">
+                                    {results.map((res: any, index: number) => (
+                                        <div
+                                            key={index}
+                                            className="flex justify-center items-center gap-2"
+                                        >
+                                            <SearchIcon className="size-3" />
+                                            {res.query}
+                                        </div>
+                                    ))}
+                                </div>
+
+                                <div className="flex w-full gap-2 justify-start items-center">
+                                    {results.map((res: any, index: number) => (
+                                        <div
+                                            key={index}
+                                            className="py-1 px-2 rounded-md border bg-neutral-800"
+                                        >
+                                            {res.result.results.map((r: any, idx: number) => (
+                                                <p key={idx}>{r.url}</p>
+                                            ))}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
                     ) : (
                         <span className="flex gap-2 justify-center items-center">
                             <LoaderCircleIcon className="animate-spin size-3" />
