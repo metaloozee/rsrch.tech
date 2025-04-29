070ea3d33ebb76e484844719fbd4e99ec6aeb5b2
metaloozee <ayanparkar123@gmail.com>
2025-04-29 21:09:35 +0530

    changed the llm model

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -23,11 +23,11 @@ export const maxDuration = 60;
 
 const tvly = tavily({ apiKey: env.TAVILY_API_KEY });
 
-// const smallModel = mistral('mistral-small-latest');
-// const largeModel = mistral('mistral-small-latest');
+const smallModel = mistral('mistral-small-latest');
+const largeModel = mistral('mistral-large-latest');
 
-const smallModel = google('gemini-2.5-flash-preview-04-17');
-const largeModel = google('gemini-2.5-flash-preview-04-17');
+// const smallModel = google('gemini-2.5-flash-preview-04-17');
+// const largeModel = google('gemini-2.5-flash-preview-04-17');
 
 export interface SearchResult {
     query: string;
